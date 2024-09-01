779c937ad864b34bf56cfd7b1082dcdb99437581
fleimkeipa <adem_sahin12@hotmail.com>
2024-09-02 00:49:21 +0300

    Change struct sort

--- model/challenge.go
+++ model/challenge.go
@@ -2,11 +2,6 @@ package model
 
 import "time"
 
-type ChallengeRequest struct {
-	ID   string `bson:"_id,omitempty"` // MongoDB ObjectID, primary key
-	Name string `bson:"name" json:"name"`
-}
-
 type Challenge struct {
 	ID        string    `bson:"_id,omitempty"` // MongoDB ObjectID, primary key
 	Name      string    `bson:"name" json:"name"`
@@ -14,6 +9,11 @@ type Challenge struct {
 	CreatedAt time.Time `bson:"created_at" json:"created_at"`
 	UpdatedAt time.Time `bson:"updated_at,omitempty" json:"updated_at,omitempty"`
 	DeletedAt time.Time `bson:"deleted_at,omitempty" json:"deleted_at,omitempty"`
+}
+
+type ChallengeRequest struct {
+	ID   string `bson:"_id,omitempty"` // MongoDB ObjectID, primary key
+	Name string `bson:"name" json:"name"`
 }
 
 type ChallengeFindOpts struct {
